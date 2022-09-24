85dc4c9b5e4e84a18973942ed215e4aeb60703e5
Ogunnoiki Adetokunbo <adetokunboogunnoiki28@gmail.com>
2022-09-25 00:57:49 +0100

    THE API WORKS NOW

--- src/components/waitlist/waitlist.jsx
+++ src/components/waitlist/waitlist.jsx
@@ -120,7 +120,7 @@ const Waitlist = () => {
           <div className="">
             <h1 className="">
               Get connected with people traveling to the same destination as
-              your gods.
+              your goods.
             </h1>
           </div>
           <div className="">
